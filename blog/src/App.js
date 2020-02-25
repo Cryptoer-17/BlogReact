25ca74b2e3707e37d65f800907148a38efec430f
monikaklim <monyklim@gmail.com>
2020-02-25 14:19:22 +0100

    added title of the page

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Homepage from './containers/Homepage/Homepage';
+
 
 function App() {
   return (
     <div className="App">
-  
-   
-       
+    <Homepage/>
+     
     </div>
   );
 }
