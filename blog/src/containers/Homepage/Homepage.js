c629a84b2b8740dd39c71c64e354d05cbdb5b030
monikaklim <monyklim@gmail.com>
2020-02-25 17:34:39 +0100

    added navigation links in the homepage

--- blog/src/containers/Homepage/Homepage.js
+++ blog/src/containers/Homepage/Homepage.js
@@ -14,20 +14,27 @@ return(
 
 <div className = {classes.Homepage}>
 
-<h1>Blog</h1>
+<div className ={classes.BarraNavigazione}>
+<a href ="" className = {classes.Link}><i className="material-icons">home</i> </a>
+<a href ="" className = {classes.Link}><i className="material-icons" style = {{verticalAlign:'middle'}}>add_box</i> </a>
+<a href ="" className = {classes.LoginLink}> Login </a>
+
+</div>
+
+<h1 className = {classes.Titolo}>Blog</h1>
 
 <Articolo>
-<p>The continuous evolution of any technology is often accompanied by the greater risks associated with it. 
-                 The same happened on the Internet as well. It has been a boon for every existing technology. 
-                 Manual working has been greatly reduced with the automation brought about with the internet. 
-                 But the Cyber Security threats that are rising with it are certainly impossible to eliminate completely.</p>
+   The continuous evolution of any technology is often accompanied by the greater risks associated with it. 
+   The same happened on the Internet as well. It has been a boon for every existing technology. 
+   Manual working has been greatly reduced with the automation brought about with the internet. 
+   But the Cyber Security threats that are rising with it are certainly impossible to eliminate completely.
 </Articolo>
 
-<Articolo>testo di prova</Articolo>
+<Articolo>Testo di prova</Articolo>
 
 <Articolo>sadgfdsgdgdfshfdhfdhghgfdg</Articolo>
 
-<Articolo>hello world</Articolo>
+<Articolo>Hello world</Articolo>
 
 <Articolo>qwertyuiop</Articolo>
 
