20ede52784f83e78622a4011a00689a92d688308
monikaklim <monyklim@gmail.com>
2020-02-26 12:28:02 +0100

    created functions to filter articles by category or tag

--- blog/src/containers/RisultatiRicerca/RisultatiRicerca.js
+++ blog/src/containers/RisultatiRicerca/RisultatiRicerca.js
@@ -4,12 +4,42 @@ import classes from './RisultatiRicerca.module.css';
 class RisultatiRicerca extends Component{
 
     state = {
-        cerca:""
+        cerca:"",
+        articoli:[
+        {
+            categoria: 'test',
+            tags:['prova','react']
+        },
+        {
+            categoria: 'test',
+            tags:['ciao','react']
+        },
+        {
+            categoria: 'prova',
+            tags:['ciao','react']
+        }
+        ]
     }
 
 
     render(){
+        console.log(this.state)
 
+let risultati = [];
+
+       const displayCategoryResultsHandler = (cerca) =>{
+
+           risultati= this.state.articoli.filter(art => art.categoria === cerca)
+           console.log(risultati.map(r => r))
+
+        }
+
+       const displayTagResultsHandler = (cerca) =>{
+
+            risultati = this.state.articoli.filter(art =>  art.tags.indexOf(cerca)>= 0)
+            console.log(risultati.map(r => r))
+
+        }
 
 
 
@@ -23,22 +53,22 @@ class RisultatiRicerca extends Component{
 
                 <div className = {classes.OpzioniRicerca}>
                     <p>Filtra per </p>
-                <a href = "">Categoria</a> | <a href = "">Tag</a>
+                <a  onClick = {() =>displayCategoryResultsHandler(this.state.cerca)}>Categoria</a> | <a  onClick = {() =>displayTagResultsHandler(this.state.cerca)}>Tag</a>
                 <hr  className = {classes.Divisore} />
                 </div>
 
 
             <div className = {classes.ContainerRisultati}>
 
-
-
+            {/* risultati */}
+                
             </div>
 
 
             </div>
    
         );
-
-    }
+     }
+    
 }
 export default RisultatiRicerca;
