a476ed9424aa9a5235fe577baefdf80ace906a05
monikaklim <monyklim@gmail.com>
2020-02-26 11:11:20 +0100

    created page to show display search results

---
+++ blog/src/containers/RisultatiRicerca/RisultatiRicerca.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import classes from './RisultatiRicerca.module.css';
+
+class RisultatiRicerca extends Component{
+
+    render(){
+
+
+
+
+
+        return(
+            <div className = {classes.RisultatiRicerca}>
+
+
+
+
+
+
+
+            </div>
+   
+        );
+
+    }
+}
+export default RisultatiRicerca;
