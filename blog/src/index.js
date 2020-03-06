c3a6cebadc072350f7268584902f6cb6fd6d95ca
monikaklim <monyklim@gmail.com>
2020-03-06 11:54:23 +0100

    imported reducer login in index.js

--- blog/src/index.js
+++ blog/src/index.js
@@ -7,13 +7,14 @@ import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware,compose, combineReducers} from 'redux';
 import reducerArticolo from './store/reducers/reducerArticolo';
+import reducerLogin from './store/reducers/reducerLogin';
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ?  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ :null|| compose;
 
 const rootReducer = combineReducers(
     {
-        articolo : reducerArticolo
-        //reducers
+        articolo : reducerArticolo,
+        login: reducerLogin
     }
 );
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
