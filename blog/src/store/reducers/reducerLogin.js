994ab8e8a649bb363b6c8674c14db22dde6b773a
monikaklim <monyklim@gmail.com>
2020-03-06 10:09:32 +0100

    created reducer

---
+++ blog/src/store/reducers/reducerLogin.js
@@ -0,0 +1,40 @@
+import * as actionTypes from '../actions/actionTypes';
+import updateObject from '../../utility/updateObject';
+
+const initialState = {
+    token: null,
+    userId: null,
+    error:null,
+    loading:null
+}
+
+const loginStart = (state,action) =>{
+
+}
+
+
+const loginFail= (state,action) =>{
+    
+}
+
+const loginSuccess = (state,action) =>{
+    
+}
+
+const logout = (state,action) =>{
+    
+}
+
+const reducer = (state = initialState,action) => {
+
+    switch(action.type){
+        case actionTypes.LOGIN_START: return loginStart(state,action);
+        case actionTypes.LOGIN_SUCCESS: return loginSuccess(state,action);   
+        case actionTypes.LOGIN_FAIL: return loginFail(state,action);  
+        case actionTypes.LOGOUT: return logout(state,action);  
+        default: return state;
+
+    }
+};
+
+export default reducer;
