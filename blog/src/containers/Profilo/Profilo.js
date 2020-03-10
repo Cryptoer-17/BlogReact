4843e4443385f41fb0349529e1a9125a936bd42a
Andrea17 <andrew.iacoo@gmail.com>
2020-03-10 10:29:44 +0100

    inserito titolo profilo persona

---
+++ blog/src/containers/Profilo/Profilo.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import classes from './Profilo.module.css';
+
+class Profilo extends Component{
+render(){
+
+    return(
+        <div className={classes.Profilo}>
+            <h1>Profilo Persona</h1>
+        </div>
+    );
+}
+
+}
+
+export default Profilo;
