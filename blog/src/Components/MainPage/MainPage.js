e68fec99f1c1098b9530273ad5b5165251e93576
monikaklim <monyklim@gmail.com>
2020-03-10 09:51:22 +0100

    styled main page

--- blog/src/Components/MainPage/MainPage.js
+++ blog/src/Components/MainPage/MainPage.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React  from 'react';
 import classes from './MainPage.module.css';
 
 const MainPage= () =>{
+
+  
     return( 
 
-        <div className ={classes.MainPage} >
-                <div  style = {{backgroundImage:"linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('../../assests/images/bg.jpg')", width:'100%',height:'100%'}}>
-   
-        <button>REGISTRATI ORA</button>
-        <p>o accedi se hai già un account</p>
-        </div>
+        <div className ={classes.MainPage}  >
+               <p className ={classes.Testo} >Crea il tuo blog </p>
+                <button className ={classes.Registrazione} onClick = {() => document.getElementById("loginButton").click()}> INIZIA ORA</button>
+                <div className = {classes.Img}> </div>
         </div>
     );
 }
