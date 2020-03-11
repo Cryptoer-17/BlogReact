ac0f0f73f95d4cc4d825fa5ce1e5af682bb4dcef
Andrea17 <andrew.iacoo@gmail.com>
2020-03-11 08:46:46 +0100

    build profil page

--- blog/src/containers/Profilo/Profilo.js
+++ blog/src/containers/Profilo/Profilo.js
@@ -11,7 +11,8 @@ class Profilo extends Component{
     state={
         nome:"",
         anteprimaImg:null,
-        presentazione:null
+        presentazione:null,
+        modificaDati:null
     }
 
 componentDidMount(){
@@ -28,9 +29,11 @@ handlerClickPresentazione(){
     this.setState({presentazione : false})
 }
 
-handlerRequest(){
+handlerModificaDati(){
 
    console.log("modifica dati");
+    this.setState({modificaDati: !this.state.modificaDati})
+
 }
 
 
@@ -47,7 +50,7 @@ convertFile = (e)=>  {
 
 render(){
 
-    let {anteprimaImg,presentazione} = this.state;
+    let {anteprimaImg,presentazione,modificaDati} = this.state;
     let email;
     email = localStorage.getItem('email');
 
@@ -59,6 +62,28 @@ render(){
         presentazioneVisualizzata= <button className={classes.BtnPresentazione} onClick={()=>this.handlerClickPresentazione()}><i>Aggiungi una breve presentazione</i></button> 
         : presentazione===false && ((presentazioneVisualizzata = <input type="text"></input>) && (btnInviaInfo = <button className={classes.ButtonEmail} >Invia breve presentazione</button>))
     } 
+
+
+    let pageModificaDati =  (<div className={classes.ModificaDati}>
+    <h3>MODIFICA I TUOI DATI</h3>
+   
+        <p>Nome:<input type="text"  className={classes.Input} placeholder="nome" onChange={(event) => this.HandlerChange(event)} value={this.state.nome}></input>
+        Cognome:<input type="text" placeholder="cognome" className={classes.Input}></input>
+        Data Nascita<input type="date" placeholder="data nascita" className={classes.Input}></input></p>
+        <p>
+         Sesso: <label>M</label><input type="radio" name="male" value="M"/>
+                <label>F</label><input type="radio" name="male" value="F"/>
+        </p>
+        <div className = {classes.InputImg}>
+        <input  id = "inputFile" type = "file" accept="image/png,image/gif,image/jpeg, image/jpg" onChange={ event =>this.convertFile(event.target.files[0]) } style = {{display:'none', visibility:'hidden',zIndex:'-200'}}/>
+
+        <button className = {classes.CaricaImgButton} onClick = {() => document.getElementById("inputFile").click() }> <i className="material-icons"  style = {{verticalAlign:'middle'}}>photo_camera</i> Carica foto profilo</button>
+       
+        { anteprimaImg ?  anteprimaImg : null}
+        </div>
+
+    </div>);
+
 
     let articoliVisualizzati;
     articoliVisualizzati = personal_article.map((art) =>{
@@ -93,32 +118,15 @@ render(){
                 {presentazioneVisualizzata}
                 {btnInviaInfo}
             </div>
+           
             <div className={classes.DatiPersonali}>  
                 <h3>DATI PERSONALI</h3> 
                 <div>
                 Email : {email}
-                <button className={classes.ButtonEmail} onClick={this.handlerRequest}><MdEmail style={{verticalAlign: 'middle'}}/> Modifica Dati</button>     
+                <button className={classes.ButtonEmail} onClick={() =>this.handlerModificaDati()}><MdEmail style={{verticalAlign: 'middle'}}/> Modifica Dati</button>     
                 </div>     
             </div>
-            <div className={classes.ModificaDati}>
-            <h3>MODIFICA I TUOI DATI</h3>
-           
-                <p>Nome:<input type="text"  className={classes.Input} placeholder="nome" onChange={(event) => this.HandlerChange(event)} value={this.state.nome}></input>
-                Cognome:<input type="text" placeholder="cognome" className={classes.Input}></input>
-                Data Nascita<input type="date" placeholder="data nascita" className={classes.Input}></input></p>
-                <p>
-                 Sesso: <label>M</label><input type="radio" name="male" value="M"/>
-                        <label>F</label><input type="radio" name="male" value="F"/>
-                </p>
-                <div className = {classes.InputImg}>
-                <input  id = "inputFile" type = "file" accept="image/png,image/gif,image/jpeg, image/jpg" onChange={ event =>this.convertFile(event.target.files[0]) } style = {{display:'none', visibility:'hidden',zIndex:'-200'}}/>
-
-                <button className = {classes.CaricaImgButton} onClick = {() => document.getElementById("inputFile").click() }> <i className="material-icons"  style = {{verticalAlign:'middle'}}>photo_camera</i> Carica foto profilo</button>
-               
-                { anteprimaImg ?  anteprimaImg : null}
-            </div>
-        
-            </div>
+            {(modificaDati) ? pageModificaDati : null}           
 
             {articoliVisualizzati}
         </div>
