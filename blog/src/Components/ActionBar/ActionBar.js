d6372aadc8f7a7dc1c15d2765501c53d68323d14
Andrea17 <andrew.iacoo@gmail.com>
2020-03-17 16:26:26 +0100

    build profil page

--- blog/src/Components/ActionBar/ActionBar.js
+++ blog/src/Components/ActionBar/ActionBar.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './ActionBar.module.css';
 import { FaHeart,FaRegCommentDots, FaEllipsisH } from "react-icons/fa";
 
 
+
 const ActionBar = (props) =>{
+
+    const [showMenu,setShowMenu] = useState(true);
+
+
+    const showPageMessage = () =>{
+        setShowMenu(!showMenu);
+    }
+
+
     return(
         <div className ={classes.ActionBar}>
         
@@ -13,8 +23,14 @@ const ActionBar = (props) =>{
                 <FaRegCommentDots className={classes.Comments}  />
                 </div>
 
-                 <div className= {classes.MoreDiv}>
-                <FaEllipsisH className={classes.More}/>
+
+                 <div className= {classes.MoreDiv} >
+                     <FaEllipsisH className={classes.More} onClick={showPageMessage}/>
+                    <div class={classes.DropdownContent}  style={showMenu ? {display:'block'} : null} >
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                    </div>
                 </div>
 
 
